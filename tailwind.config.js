bf3095a96a82bc57d91f57bccc6a6037c7e723f9
jeff <somuadinaokoli219@gmail.com>
2022-02-12 03:18:33 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        meetBgGreen: "#013243",
+        meetTextColor: "#32f5c8",
+        meetGrey: "#a9bdc6",
+      },
+      fontSize: {
+        "10xl": "158px",
+      },
+    },
+  },
+  plugins: [],
+};
